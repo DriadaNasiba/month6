be3f3ac5d9ef7cd5d9cef4d7b32f61c0c9f58e44
dridanasiba <nasiba.mabetova11@gmail.com>
2025-07-27 21:46:55 +0600

    hw4

--- users/models.py
+++ users/models.py
@@ -5,6 +5,10 @@ from users.managers import CustomUserMnager
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=50, null=True, blank=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+    avatar = models.URLField(blank=True, null=True)
+
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
